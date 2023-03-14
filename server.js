c10d618d091446c090262b1eed705dd91bdb5002
victor3213 <efrosvictor424@gmail.com>
2023-03-14 19:23:09 +0000

    create file for room and id for room

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server()
+const { v4: uuidv4 } = require('uuid')
 
 const PORT = process.env.PORT || 5001
+
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
 
 
 app.listen(PORT, () => {
